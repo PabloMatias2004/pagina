9ccf6b751a09bedc03e934eec3e1351da7122481
PabloMatias2004 <pablomatias3810@gmail.com>
2019-12-14 15:30:55 -0600

    pagina

---
+++ vinedo/js/script.js
@@ -0,0 +1,15 @@
+var A = angular.module("A", [])
+
+A.controller("Principal", function($scope){
+//Area circulo
+	$scope.diametro = function(Y){
+		$scope.uvas = Y;
+		if ($scope.uvas.A>$scope.uvas.B) {
+			$scope.precioA=5;
+			$scope.precioA=10;
+			$scope.totalcarga=
+		}
+		
+		alert($scope.totalcarga+ "Realizado")
+	}
+})
